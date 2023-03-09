eb7270e8be36c7c03c7640ddd587e4b791c25e72
D8M <davidmahon0000@gmail.com>
2023-03-09 20:25:35 +0000

    A Hello World in a couple of different languages

---
+++ src/DifferentLanguageGreeting.java
@@ -0,0 +1,13 @@
+
+public class DifferentLanguageGreeting {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Dia dhuit a chairde");
+		System.out.println("Hallo Welt");
+		System.out.println("Salam Midiri ");
+		System.out.println("Bounjour le Monde");
+
+	}
+
+}
