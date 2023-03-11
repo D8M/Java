7630dfd8ee59f8f79fd62a647a634999bcaad762
D8M <davidmahon0000@gmail.com>
2023-03-11 13:46:21 +0000

    Updated Java Method

--- src/JavaMethods.java
+++ src/JavaMethods.java
@@ -5,25 +5,27 @@ public class JavaMethods {
 	
 		
 		
-		int levelCompleted = 500;
-		calculateScore(true, 800,  700 , 100);
 		
-		calculateScore(true, 800,  1000 , 100);
+		calculateScore(true, 800,  700 , 100);		
 		
 		
+		System.out.println("Final score: " + calculateScore(true, 800,  1000 , 100));
+		
+		
+	 } // Main method end
 
-	}	
+
+
 	
-public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 			
 	int finalScore = score;
 	if(gameOver) {
 		finalScore += (levelCompleted * bonus);
-				finalScore += 1000;
-					System.out.println("Final score: " + finalScore);
-	}
+				finalScore += 1000;	} // If statement end	
 	
+	return finalScore;	
 		
-	}
+	} // Method end
 
-}
+} // Class end
