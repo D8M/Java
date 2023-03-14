6e1453da216707e1c833478c1256027e25ff12b2
D8M <davidmahon0000@gmail.com>
2023-03-14 15:21:11 +0000

    Test Class code for coding exercise one,lesson 51 coding exercises

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,2 +1,16 @@
 public class TestClass {
+
+
+    public static void checkNumber(int userNum) {
+        if (userNum > 0) {
+            System.out.print("positive\n");
+        } else if (userNum < 0) {
+            System.out.print("negative\n");
+        } else {
+            System.out.print("zero\n");
+        }
+    }
+
 }
+
+
