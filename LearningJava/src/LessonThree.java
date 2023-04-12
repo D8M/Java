3a870e5265e7c4f468d5702f6c22fbe75fdae0e3
D8M <davidmahon0000@gmail.com>
2023-04-13 00:35:26 +0100

    Lesson 3 is covers Java fundamentals from Derek Banas Java course on YouTube

--- LearningJava/src/LessonThree.java
+++ LearningJava/src/LessonThree.java
@@ -2,7 +2,38 @@ public class LessonThree {
 
     public static void main(String[] args){
 
-        int randomNum = (int) (Math.random() * 50);
+        int randomNum = (int) (Math.random() * 100);
+
+        if (randomNum < 52)
+        {
+            System.out.print("The random number " + randomNum + " is less than 52");
+        }
+        else if (randomNum != 40)
+        {
+            System.out.print("The random number " + randomNum + " is greater than 40");
+        }
+        else
+        {
+            System.out.print("Some random stuff printed here ;)");
+        }
+
+        if(!(false))
+        {
+            System.out.println("\nI turned false into true");
+        }
+
+        if((false) | (true))
+        {
+            System.out.println("One is true...");
+        }
+
+         int valueOne = randomNum;
+
+         int valueTwo = randomNum;
+
+         int biggestValue = (valueOne > valueTwo) ? valueOne : valueTwo;
+            System.out.println(biggestValue + "+" + valueOne + "+" + valueTwo);
+
 
 
     }
---
+++ LearningJava/src/SwitchCase.java
@@ -0,0 +1,35 @@
+public class SwitchCase {
+
+    public static void main(String[] args){
+
+        char theGrade = 'B';
+
+        switch (theGrade)
+        {
+            case 'A':
+                System.out.println("Great Job");
+                break;
+            case 'B':
+                System.out.println("Good Job");
+                break;
+            case 'C':
+                System.out.println("Ok work");
+                break;
+            case 'D':
+                System.out.println("Needs Improvement");
+                break;
+            default:
+                System.out.println("Really?");
+                break;
+        }
+
+
+
+
+
+
+
+
+    }
+
+}
